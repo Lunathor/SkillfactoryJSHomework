65dfbb070717f28106b8cfc51af70c8a42d4400b
Lunathor <mrcoopgmz@gmail.com>
2024-10-23 10:54:41 +0300

    Задание 3

--- index.js
+++ index.js
@@ -126,3 +126,15 @@
 //     return result;
 // }
 // defineSimple(+prompt('Введите число'));
+// Задание 3
+function getSumFunc(firstOperand){
+    return function(secondOperand){
+        return firstOperand + secondOperand;
+    }
+}
+const sum = getSumFunc(+prompt('Введите первый операнд'));
+const result = sum(+prompt("Введите второй операнд")); // Взял функцию prompt чтобы были не заранее подготовленные числа а те которые введет пользователь
+console.log(`Сумма полученных чисел ${result}`);
+alert(`Сумма полученных чисел ${result}`); // Выведем в консоль и на экран если проверка будет в браузере
+
+
