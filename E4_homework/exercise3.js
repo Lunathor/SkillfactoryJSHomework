d5f67b83bac4832b8196d2e4452d4a53453e34d9
Lunathor <mrcoopgmz@gmail.com>
2024-10-31 00:25:11 +0300

    Задание 3 модуля 4

--- E4_homework/exercise3.js
+++ E4_homework/exercise3.js
@@ -2,3 +2,12 @@
  * Задание 3.
  * Написать функцию, которая создает пустой объект, но без прототипа.
  */
+function objCreator() {
+    return Object.create(null);
+}
+
+const noProtoObj = objCreator();
+
+console.log(noProtoObj);
+
+console.log(Object.getPrototypeOf(noProtoObj));
